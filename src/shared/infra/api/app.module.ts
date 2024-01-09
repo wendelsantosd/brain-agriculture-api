93704cd28372549652414adc10c7d10d53b0918b
wendelsantosd <wendelwcsantos@gmail.com>
2024-01-09 17:10:53 -0300

    feat: config database

--- src/shared/infra/api/app.module.ts
+++ src/shared/infra/api/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from '../db/prisma.service';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
