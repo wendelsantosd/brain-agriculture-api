889dcbd04480446ddc2d45850abaa552ef8748a9
wendelsantosd <wendelwcsantos@gmail.com>
2024-01-09 15:57:41 -0300

    chore: config ts

--- src/shared/infra/api/index.ts
+++ src/shared/infra/api/index.ts
@@ -1,8 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { server } from '@shared/config/env/server';
 import { json } from 'express';
-import { server } from 'src/shared/config/env/server';
 import { AppModule } from './app.module';
 
 export async function bootstrapAPI() {
